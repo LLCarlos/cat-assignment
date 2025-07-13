fac82c1b1fad7e9a6eb9d93c812d3fdca8dfc1c5
Leonardo_luis_Carlos <Leonardo_luis_carlos@dell.com>
2025-07-13 19:53:28 -0300

    Documentation

--- enrich_movie.py
+++ enrich_movie.py
@@ -3,15 +3,18 @@ import boto3
 import os
 import requests
 
+# env variables
 OMDB_API_KEY = os.environ['OMDB_API_KEY']
 S3_BUCKET = os.environ['S3_BUCKET_URL']
 SQS_URL = os.environ['SQS_URL']
 
+# clients
 s3 = boto3.client('s3')
 sqs = boto3.client('sqs')
 
 
 def lambda_handler(event, context):
+    # get movies from sqs queue. For each movie, get omdb data and save to s3
     response = sqs.receive_message(
         QueueUrl=SQS_URL,
         AttributeNames=[
@@ -24,15 +27,20 @@ def lambda_handler(event, context):
         VisibilityTimeout=0,
         WaitTimeSeconds=20
     )
+    # if no messages, exit
+    if 'Messages' not in response:
+        return {"statusCode": 404, "body": "No messages in queue"}
     if 'Messages' in response:
         for message in response['Messages']:
-            print(message)
+            #fetch the movie 
             movie = json.loads(message['Body'])
+            # fetch the id
             imdb_id = movie['id']
 
+            # get omdb data from their api
             response = requests.get(f"https://www.omdbapi.com/?apikey={OMDB_API_KEY}&i={imdb_id}")
             movie['omdb'] = response.json()
-
+            # save to s3
             s3.put_object(
                 Bucket=S3_BUCKET,
                 Key=f"{imdb_id}.json",
@@ -41,10 +49,10 @@ def lambda_handler(event, context):
             )
 
             receipt_handle = message['ReceiptHandle']
-
+            # clear queue
             sqs.delete_message(
                 QueueUrl=SQS_URL,
                 ReceiptHandle=receipt_handle
             )
-
+    # if all good, return 200
     return {"statusCode": 200, "body": "Movie enriched and saved"}
--- get_movies.py
+++ get_movies.py
@@ -9,10 +9,11 @@ SQS_QUEUE_URL = 'test'
 def lambda_handler(event, context):
     response = requests.get("https://top-movies.s3.eu-central-1.amazonaws.com/Top250Movies.json")
     movies = json.loads(response.text)
-
+    # get top 10 movies in the items response. since its already sorted, we can just take the first 10
     top_10 = movies['items'][0:10]
-
+    # client for sqs
     sqs = boto3.client('sqs')
+    # loop to send the messages
     for movie in top_10:
         sqs.send_message(
             QueueUrl=SQS_QUEUE_URL,
